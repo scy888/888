cfcfc4fb2d0fc3f1868a6ba849245af6c8b3d081
MySetsuna <1286783182@qq.com>
2019-05-21 18:48:08 +0800

    略做修改

--- pinyougou_parent/pinyougou_portal_web/src/main/webapp/js/controller/indexController.js
+++ pinyougou_parent/pinyougou_portal_web/src/main/webapp/js/controller/indexController.js
@@ -9,6 +9,14 @@ app.controller("indexController",function ($scope,contentService) {
         contentService.findByCategoryId(1).success(function (response) {
             $scope.contentList[1] = response;
         })
+        //加载轮播图广告
+        contentService.findByCategoryId(2).success(function (response) {
+            $scope.contentList[2] = response;
+        })
+        //加载轮播图广告
+        contentService.findByCategoryId(3).success(function (response) {
+            $scope.contentList[3] = response;
+        })
     }
 
     $scope.keywords = "";
